0a0e6051c9c5d07723af8f846fd710f7c8640147
justJuste <jules.longin1@gmail.com>
2024-12-01 17:29:39 +0100

    remove_if that works and is normed

--- C12/ex12/ft_list_remove_if.c
+++ C12/ex12/ft_list_remove_if.c
@@ -15,30 +15,25 @@ void	ft_list_remove_if(t_list **begin_list,
 		void *data_ref, int (*cmp)(), void (*free_fct)(void *))
 {
 	t_list		*cur;
-	t_list		*tmp;
 
+	if (!begin_list || !(*begin_list))
+		return ;
 	cur = *begin_list;
-	while (cur && cur->next)
-	{
-		if (cmp(cur->next->data, data_ref) == 0)
-		{
-			tmp = cur->next;
-			cur->next = cur->next->next;
-			free_fct(tmp->data);
-			free(tmp);
-		}
-		cur = cur->next;
-	}
-	cur = *begin_list;
-	if (cur && cmp(cur->data, data_ref) == 0)
+	if (cmp(cur->data, data_ref) == 0)
 	{
 		*begin_list = cur->next;
 		free_fct(cur->data);
 		free(cur);
+		ft_list_remove_if(begin_list, data_ref, cmp);
+	}
+	else
+	{
+		cur = *begin_list;
+		ft_list_remove_if(&cur->next, data_ref, cmp);
 	}
 }
 
-/*	2 en 1 :
+/*	2 en 1 : iterative
 
 void	ft_list_remove_if(t_list **begin_list, void *data_ref,
 		int (*cmp)(), void (*free_fct)(void *))
